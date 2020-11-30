84429d553696afc097c44cd8274361cc06c2068c
page hunt <pagelhunt@gmail.com>
2020-11-30 01:52:00 -0700

    added config for c# console proj

---
+++ testing/testing/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "A bunch of things";
+
+            Console.WriteLine(phrase);
+
+            Console.ReadLine();
+        }
+    }
+}
